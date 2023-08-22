b0cc057fe9990c414cce1f7dd8657841d00a890b
Антон <sergeev.tony@ya.ru>
2023-08-22 19:46:05 +0300

    add new articles

---
+++ src/main/java/com/sav/blog/repo/PostRepository.java
@@ -0,0 +1,8 @@
+package com.sav.blog.repo;
+
+import com.sav.blog.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+}
